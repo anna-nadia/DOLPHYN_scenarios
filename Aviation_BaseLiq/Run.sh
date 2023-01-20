609fbc5af8c6008065005527995245323c550d2d
cybulsky <acybulsky@gmail.com>
2023-01-20 18:52:49 -0500

    v1
    Base+Aviation Demand scenario

---
+++ Aviation_BaseLiq/Run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=AC_Liq               # create a short name for your job
+#SBATCH --nodes=1                           # node count
+#SBATCH --ntasks=1                          # total number of tasks across all nodes
+#SBATCH --cpus-per-task=4                   # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=10G                    # memory per cpu-core
+#SBATCH --time=12:00:00                     # total run time limit (HH:MM:SS)
+#SBATCH --output="test.out"
+#SBATCH --error="test.err"
+#SBATCH --mail-type=FAIL                    # notifications for job done & fail
+#SBATCH --mail-user=cybulsky@mit.edu         # send-to address
+
+source /etc/profile
+module load julia/1.7.3
+module load gurobi/gurobi-951
+
+export GUROBI_HOME="/home/gridsan/acybulsky/gurobi952/linux64"
+
+julia Run.jl
+
+date
